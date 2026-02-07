9dd3c77aef921d73f4e6b41f2a4239119c0c4c85
Shoko <a.breyton.pro@gmail.com>
2026-02-07 18:04:44 +0100

    Ajout de mouvements simple

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import pygame
 import time
 
+L = 1300
+H = 800
+
 def run():
     pygame.init()
     clock = pygame.time.Clock()
     pygame.display.set_caption("Get What U Need")
-    screen = pygame.display.set_mode((1300, 800))
+    screen = pygame.display.set_mode((L, H))
     
+    x = L // 2
+    y = H // 2
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-                
+        
+        touches = pygame.key.get_pressed()
+        if touches[pygame.K_q]: x -= 3
+        if touches[pygame.K_d]: x += 3
+        if touches[pygame.K_z]: y -= 3
+        if touches[pygame.K_s]: y += 3
+
         pygame.display.set_caption("Get What U Need - {}".format(time.strftime("%Hh%M")))
         screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (0, 0, 0), (x, y, 100, 100))
         pygame.display.flip()
 
         clock.tick(120)
